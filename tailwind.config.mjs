32c2ae3bf47d61aa7f2ae68b4ec31bd8565f50f0
karenamg <kamoran.19@est.ucab.edu.ve>
2024-03-27 13:57:12 -0400

    Navbar responsive

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,39 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  darkMode: "class",
+  theme: {
+    extend: {
+      fontFamily: {
+        bjorn: ["Bjorn", "NexaBold", "NexaLight"],
+      },
+      colors: {
+        blue: "#185A9D",
+        blueLight: "#2E8CE2",
+        green: "#2EE2AC",
+        red: "#EA2662",
+        dark: "#101C28",
+        darker: "#0C141C",
+        light: "#DBEEFF",
+
+        primary: "#EB00D7",
+        primaryDark: "#460040",
+        primaryDarkOne: "#460040",
+        primaryDarkTwo: "#33002F",
+        primaryDarkThree: "#270025",
+        secondary: "#00B8E7",
+        secondaryDark: "#2192B0",
+        tertiary: "#EBC500",
+        tertiaryDark: "#463301",
+        grayDark: "#424242",
+        grayLight: "#767779",
+        black: "#000000",
+        blackOne: "#000000",
+        blackTwo: "#0E0E0E",
+        white: "#FFFFFF",
+        text: "#28262C",
+      },
+    },
+  },
+  plugins: [],
+};
